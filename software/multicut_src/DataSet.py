Fix segmentation masking

software/multicut_src/DataSet.py
@@ -11,24 +11,28 @@ from tools import cacher_hdf5, cache_name
 from tools import edges_to_volume_from_uvs_in_plane, edges_to_volume_from_uvs_between_plane
 from tools import edges_to_volumes_for_skip_edges
 
+from defect_handling import modified_edge_indications, modified_adjacency
+from defect_handling import modified_edge_gt, modified_edge_gt_fuzzy
+from defect_handling import get_skip_edges, get_skip_ranges, get_skip_starts
+
 from ExperimentSettings import ExperimentSettings
 from feature_impls import topology_features_impl
 
 # if build from source and not a conda pkg, we assume that we have cplex
 try:
-    import nifty
     import nifty.graph.rag as nrag
     import nifty.ground_truth as ngt
+    import nifty.segmentation as nseg
 except ImportError:
     try:
-        import nifty_with_cplex as nifty  # conda version build with cplex
         import nifty_with_cplex.graph.rag as nrag
         import nifty_with_cplex.ground_truth as ngt
+        import nifty_with_cplex.segmentation as nseg
     except ImportError:
         try:
-            import nifty_with_gurobi as nifty  # conda version build with gurobi
             import nifty_with_gurobi.graph.rag as nrag
             import nifty_with_gurobi.ground_truth as ngt
+            import nifty_with_gurobi.segmentation as nseg
         except ImportError:
             raise ImportError("No valid nifty version was found.")
 
@@ -449,17 +453,9 @@ class DataSet(object):
             # TODO change once we allow more general values
             assert ExperimentSettings().ignore_seg_value == 0, "Only zero ignore value supported for now"
             seg[np.logical_not(mask)] = ExperimentSettings().ignore_seg_value
-            # FIXME FIXME FIXME
-            # TODO do we need to transpose here, because otherwise fortran order messes up the flat superpixels ??
-            seg, _, _ = vigra.analysis.relabelConsecutive(
-                seg.astype('uint32'),
-                start_label=1,
-                keep_zeros=True
-            )
+            seg = nseg.connectedComponents(seg, ignoreBackground=True)
         else:
-            # FIXME FIXME FIXME
-            # TODO do we need to transpose here, because otherwise fortran order messes up the flat superpixels ??
-            seg, _, _ = vigra.analysis.relabelConsecutive(seg.astype('uint32'), start_label=0, keep_zeros=False)
+            seg = nseg.connectedComponents(seg, ignoreBackground=False)
         return seg
 
     def add_seg(self, seg_path, seg_key):
@@ -1161,7 +1157,7 @@ class DataSet(object):
         save_file = cache_name('topology_features', 'feature_folder', False, False, self, seg_id, use_2d_edges)
         vigra.writeHDF5(topo_feat_names, save_file, "topology_features_names")
 
-        return topo_feats
+        return np.nan_to_num(topo_feats)
 
     # get the names of the region features
     def topology_features_names(self, seg_id, use_2d_edges):
@@ -1421,12 +1417,12 @@ class DataSet(object):
 
         volumina_n_layer(data, labels)
 
-    def view_edge_labels(self, seg_id, with_defects=False):
-        from volumina_viewer import volumina_n_layer
+    def view_edge_labels(
+        self,
+        seg_id,
+        with_defects=False
+    ):
         from EdgeRF import mask_edges
-        from defect_handling import modified_edge_indications, modified_adjacency
-        from defect_handling import modified_edge_gt, modified_edge_gt_fuzzy
-        from defect_handling import get_skip_edges, get_skip_ranges, get_skip_starts
 
         uv_ids = modified_adjacency(self, seg_id) if with_defects else self.uv_ids(seg_id)
 
@@ -1448,23 +1444,35 @@ class DataSet(object):
         labels_debug[labels == 0.]  = 2
         labels_debug[np.logical_not(labeled)] = 5
 
+        self.view_edge_values(self, seg_id, labels_debug, with_defects)
+
+    def view_edge_values(
+        self,
+        seg_id,
+        edge_values,
+        with_defects=False
+    ):
+        from volumina_viewer import volumina_n_layer
+
+        uv_ids = modified_adjacency(self, seg_id) if with_defects else self.uv_ids(seg_id)
+
         if with_defects:
-            skip_transition = labels_debug.shape[0] - get_skip_edges(self, seg_id).shape[0]
+            skip_transition = edge_values.shape[0] - get_skip_edges(self, seg_id).shape[0]
             edge_indications = modified_edge_indications(self, seg_id)[:skip_transition]
             # get  uv ids and labels for the skip edges
             uv_skip     = uv_ids[skip_transition:]
-            labels_skip = labels_debug[skip_transition:]
+            labels_skip = edge_values[skip_transition:]
             # get uv ids and labels for the normal edges
             uv_ids      = uv_ids[:skip_transition]
-            labels_debug = labels_debug[:skip_transition]
+            edge_values = edge_values[:skip_transition]
         else:
             edge_indications = self.edge_indications(seg_id)
 
-        assert edge_indications.shape[0] == labels_debug.shape[0], \
-            "%i, %i" % (edge_indications.shape[0], labels_debug.shape[0])
+        assert edge_indications.shape[0] == edge_values.shape[0], \
+            "%i, %i" % (edge_indications.shape[0], edge_values.shape[0])
         # xy - and z - labels
-        labels_xy = labels_debug[edge_indications == 1]
-        labels_z  = labels_debug[edge_indications == 0]
+        labels_xy = edge_values[edge_indications == 1]
+        labels_z  = edge_values[edge_indications == 0]
         uv_xy = uv_ids[edge_indications == 1]
         uv_z  = uv_ids[edge_indications == 0]
 
@@ -1473,7 +1481,13 @@ class DataSet(object):
         edge_vol_z = edges_to_volume_from_uvs_between_plane(self, seg, uv_z, labels_z)
 
         raw = self.inp(0).astype('float32')
-        gt  = self.gt()
+
+        data = [raw, seg]
+        labels = ['raw', 'seg']
+
+        if self.has_gt:
+            data.append(self.gt())
+            labels.append('groundtruth')
 
         if with_defects:
             skip_ranges = get_skip_ranges(self, seg_id)
@@ -1486,15 +1500,13 @@ class DataSet(object):
                 skip_starts,
                 skip_ranges
             )
-            volumina_n_layer(
-                [raw, seg, gt, edge_vol_z, edge_vol_skip, edge_vol_xy],
-                ['raw', 'seg', 'groundtruth', 'labels_z', 'labels_skip', 'labels_xy']
-            )
+            data.extend([edge_vol_z, edge_vol_skip, edge_vol_xy])
+            labels.extend(['labels_z', 'labels_skip', 'labels_xy'])
         else:
-            volumina_n_layer(
-                [raw, seg, gt, edge_vol_z, edge_vol_z, edge_vol_xy],
-                ['raw', 'seg', 'groundtruth', 'labels_z', 'labels_xy']
-            )
+            data.extend([edge_vol_z, edge_vol_xy])
+            labels.extend(['labels_z', 'labels_xy'])
+
+        volumina_n_layer(data, labels)
 
 
 # cutout from a given Dataset, used for cutouts and tesselations

software/multicut_src/feature_impls.py
@@ -74,14 +74,16 @@ def topo_feats_slice(seg, uv_ids):
     # get the uv ids corresponding to the lines / faces
     # (== 1 cells), and map them back to the original ids
     line_uv_ids = np.array(cell_bounds[1])
-    line_uv_ids = replace_from_dict(line_uv_ids, reverse_mapping)
+    line_uv_ids = np.sort(
+        replace_from_dict(line_uv_ids, reverse_mapping),
+        axis=1
+    )
 
-    assert (np.unique(line_uv_ids) == np.unique(uv_ids)).all()
+    # assert (np.unique(line_uv_ids) == np.unique(uv_ids)).all()
     # find the mapping from the global uv-ids to the line-uv-ids
     matching_indices = find_matching_row_indices(uv_ids, line_uv_ids)
     edge_ids = matching_indices[:, 0]
     edge_ids_local = matching_indices[:, 1]
-    assert len(edge_ids) == len(edge_ids_local), "%i, %i" % (len(edge_ids), len(edge_ids_local))
 
     n_edges = len(uv_ids)
     # take care of duplicates resulting from edges made up of multiple faces
@@ -170,9 +172,8 @@ def topo_feats_xy(rag, seg, edge_indications, node_z_coords):
     # topology features  ->  6
     # faces per edge     ->  1
     n_feats = 93
-    uv_ids = rag.uvIds()[edge_indications == 1]
-
-    feats_xy = np.zeros((len(uv_ids), n_feats), dtype='float32')
+    feats_xy = np.zeros((rag.numberOfEdges, n_feats), dtype='float32')
+    uv_ids = rag.uvIds()
 
     # iterate over the slices and
     # TODO parallelize
@@ -209,12 +210,14 @@ def topo_feats_z(rag, seg, edge_indications):
     n_feats = 93
 
     # get the uv-ids of z-edges
-    z_edge_ids = np.where([edge_indications == 0])[0]
-    feats_z = np.zeros((len(z_edge_ids), n_feats), dtype='float32')
+    assert rag.numberOfEdges == len(edge_indications), "%i, %i" % (rag.numberOfEdges, len(edge_indications))
+    feats_z = np.zeros((rag.numberOfEdges, n_feats), dtype='float32')
+    z_edge_ids = np.where(edge_indications == 0)[0]
 
     coordinate_calc = nrag.ragCoordinates(rag)
-    coordinates = [coordinate_calc.edgeCoordinates(edge_id) for edge_id in z_edge_ids]
-    start_coordinates = np.array([np.min(coord[:, 0]) for coord in coordinates], dtype='uint32')
+    assert coordinate_calc.storageLengths() == rag.numberOfEdges
+    coordinates = {edge_id: coordinate_calc.edgeCoordinates(edge_id) for edge_id in z_edge_ids}
+    start_coordinates = np.array([np.min(coord[:, 0]) for coord in coordinates.values()], dtype='uint32')
     assert len(start_coordinates) == len(z_edge_ids)
 
     # iterate over the pairs of adjacent slices, map z-edges to
@@ -224,10 +227,13 @@ def topo_feats_z(rag, seg, edge_indications):
         # z-edges to segmentation:
         # first find the edges in connecting slice z to z + 1
         # then map them to a segmentation (note that z edges coresspond to areas !)
-        this_edge_ids = np.where(start_coordinates == z)[0]
+        this_edge_ids = z_edge_ids[start_coordinates == z]
         edge_seg = np.zeros(seg.shape[1:], dtype='uint32')
-        for edge_id in this_edge_ids:
-            edge_seg[coordinates[edge_id][:, 1:]] = edge_id
+
+        # we map to a segmentation consecutive starting from 0
+        for ii, edge_id in enumerate(this_edge_ids):
+            coords = coordinates[edge_id][:, 1:]
+            edge_seg[(coords[:, 0], coords[:, 1])] = ii
 
         # build a rag based on the edge segmentation to have the uv-ids
         # and extract features for the lines between the z-edges
@@ -237,10 +243,9 @@ def topo_feats_z(rag, seg, edge_indications):
 
         # map the feats back to the z-edges via averaging over the line feats
         feats = np.zeros((len(this_edge_ids), feats_lines.shape[1]), dtype='float32')
-
-        for ii, edge_id in enumerate(this_edge_ids):
-            edge_mask = (edge_uv_ids == edge_id).any(axis=1)
-            feats[ii] = np.mean(feats[edge_mask], axis=0)
+        for ii in xrange(len(this_edge_ids)):
+            edge_mask = (edge_uv_ids == ii).any(axis=1)
+            feats[ii, :] = np.mean(feats_lines[edge_mask, :], axis=0)
 
         feats_z[this_edge_ids] = feats
 
@@ -256,8 +261,8 @@ def topology_features_impl(rag, seg, edge_indications, edge_lens, node_z_coords)
 
     # merge features
     extra_features = np.zeros_like(feats_xy, dtype='float32')
-    extra_features[edge_indications == 1] = feats_xy
-    extra_features[edge_indications == 0] = feats_z
+    extra_features[edge_indications == 1] = feats_xy[edge_indications == 1]
+    extra_features[edge_indications == 0] = feats_z[edge_indications == 0]
 
     extra_names = ['blub']  # TODO proper names
     return extra_features, extra_names
@@ -294,6 +299,7 @@ if __name__ == '__main__':
         nodes_z[seg[1]] = 1
         uv_ids = rag.uvIds()
         edge_indications = (nodes_z[uv_ids[:, 0]] == nodes_z[uv_ids[:, 1]]).astype('uint8')
+        assert len(edge_indications) == rag.numberOfEdges
         return seg, rag, nodes_z, edge_indications
 
     def test_topofeats_xy():
@@ -303,8 +309,15 @@ if __name__ == '__main__':
 
     def test_topofeats_z():
         seg, rag, nodes_z, edge_indications = test_seg()
-        feats = topo_feats_z(rag, seg, nodes_z)
+        feats = topo_feats_z(rag, seg, edge_indications)
+        print feats.shape
+
+    def topofeats():
+        seg, rag, nodes_z, edge_indications = test_seg()
+        edge_lens = np.ones(rag.numberOfEdges)
+        feats, _ = topology_features_impl(rag, seg, edge_indications, edge_lens, nodes_z)
         print feats.shape
 
     # test_topofeats_xy()
-    test_topofeats_z()
+    # test_topofeats_z()
+    topofeats()