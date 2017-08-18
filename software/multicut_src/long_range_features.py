1198ed8da35dab982beb1277479804c84d958508
Constantin Pape <c.pape@gmx.net>
2017-08-17 22:26:55 -0400

    Stuff

--- software/multicut_src/long_range_features.py
+++ software/multicut_src/long_range_features.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, division
 import vigra
+import numpy as np
 
 from .ExperimentSettings import ExperimentSettings
 from .lifted_mc import compute_and_save_lifted_nh
@@ -22,7 +23,11 @@ except ImportError:
 # get the long range z adjacency
 def get_long_range_z_adjacency(ds, seg_id, lifted_range):
     rag = ds.rag(seg_id)
-    return nrag.getLongRangeAdjacency(rag, lifted_range)
+    adjacency = nrag.getLongRangeAdjacency(rag, lifted_range)
+    if ds.has_seg_mask:
+        where_uv = (adjacency != ExperimentSettings().ignore_seg_value).all(axis=1)
+        adjacency = adjacency[where_uv]
+    return adjacency
 
 
 # get features of the long range z-adjacency from affinity maps
@@ -35,17 +40,26 @@ def get_long_range_z_features(ds, seg_id, affinity_map_path, affinity_map_key, l
     rag = ds.rag(seg_id)
     adjacency = get_long_range_z_adjacency(ds, seg_id, lifted_range)
 
-    long_range_feats = nrag.accumulateLongRangeFeatures(
-        rag,
-        affinity_maps,
-        len(adjacency),
-        ExperimentSettings().n_threads
-    )
+    if ds.has_seg_mask:
+        long_range_feats = nrag.accumulateLongRangeFeatures(
+            rag,
+            affinity_maps,
+            len(adjacency),
+            ignoreSegValue=ExperimentSettings().ignore_seg_value,
+            numberOfThreads=ExperimentSettings().n_threads
+        )
+    else:
+        long_range_feats = nrag.accumulateLongRangeFeatures(
+            rag,
+            affinity_maps,
+            len(adjacency),
+            numberOfThreads=ExperimentSettings().n_threads
+        )
     return np.nan_to_num(long_range_feats)
 
 
 # match the standard lifted neighborhood (lifted-nh) to the z-adjacency (lifted-range)
-def match_to_lifted_nh(ds, seg_id, lifed_nh, lifted_range):
+def match_to_lifted_nh(ds, seg_id, lifted_nh, lifted_range):
     assert lifted_nh >= lifted_range
     uv_lifted = compute_and_save_lifted_nh(ds, seg_id, lifted_nh)
     uv_long_range = get_long_range_z_adjacency(ds, seg_id, lifted_range)
@@ -53,3 +67,8 @@ def match_to_lifted_nh(ds, seg_id, lifed_nh, lifted_range):
     matches = find_matching_row_indices(uv_long_range, uv_lifted)[:, 0]
     assert len(matches) == len(uv_long_range)
     return matches
+
+
+# learn lifted rf with long ange features
+def learn_rf_with_long_range():
+    pass
